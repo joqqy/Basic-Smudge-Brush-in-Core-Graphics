27b9ea21fa8fe9b17ca4f400341eb87469e7b9d3
Pierre Hanna <realbrush@gmail.com>
2022-08-24 19:30:32 +0200

    m

--- MaskTest/ViewController.swift
+++ MaskTest/ViewController.swift
@@ -25,21 +25,25 @@ class CanvaView: UIView {
                 
                 // If the mask is an image, then white areas are opaque, and black areas are transparent
                 // If the mas is a mask, white areas are transparent and black areas opaque.
+                // https://developer.apple.com/library/archive/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_images/dq_images.html#//apple_ref/doc/uid/TP30001066-CH212-TPXREF101
                 
                 if let mask = UIImage(named: "tigermask_1_S")?.cgImage {
                     if let masked = cg.masking(mask) {
                         
                         let rect = CGRect(origin: .zero, size: size)
                         
+                        // Save context state
                         ctx.saveGState()
                         
                         // Flip the context so that the coordinates match the default coordinate system of UIKit
+                        // https://developer.apple.com/library/archive/documentation/2DDrawing/Conceptual/DrawingPrintingiOS/HandlingImages/Images.html#//apple_ref/doc/uid/TP40010156-CH13-SW1
                         ctx.translateBy(x: 0, y: size.width)
                         ctx.scaleBy(x: 1, y: -1)
                         
-                        
+                        // Draw
                         ctx.draw(masked, in: rect)
                         
+                        // Restore context state
                         ctx.restoreGState()
                     }
                 }
