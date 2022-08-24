4ae76fcdc48d22787740cb6ecfc64c852ff3cdc0
Pierre Hanna <realbrush@gmail.com>
2022-08-24 19:45:26 +0200

    m

--- MaskTest/CanvasView.swift
+++ MaskTest/CanvasView.swift
@@ -17,41 +17,83 @@ class CanvaView: UIView {
         self.backgroundColor = .white
         
         self.image = UIImage(named: "tiger")
+        
+        drawMask()
     }
     
-    override func draw(_ rect: CGRect) {
+    // Overriding draw
+//    override func draw(_ rect: CGRect) {
+//
+//        if let ctx: CGContext = UIGraphicsGetCurrentContext() {
+//
+//            if let cg: CGImage = self.image?.cgImage,
+//               let size: CGSize = self.image?.size {
+//
+//                // If the mask is an image, then white areas are opaque, and black areas are transparent
+//                // If the mas is a mask, white areas are transparent and black areas opaque.
+//                // https://developer.apple.com/library/archive/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_images/dq_images.html#//apple_ref/doc/uid/TP30001066-CH212-TPXREF101
+//
+//                if let mask = UIImage(named: "tigermask_1_S")?.cgImage {
+//                    if let masked = cg.masking(mask) {
+//
+//                        let rect: CGRect = CGRect(origin: .zero, size: size)
+//
+//                        // Save context state
+//                        ctx.saveGState()
+//
+//                        // Flip the context so that the coordinates match the default coordinate system of UIKit
+//                        // https://developer.apple.com/library/archive/documentation/2DDrawing/Conceptual/DrawingPrintingiOS/HandlingImages/Images.html#//apple_ref/doc/uid/TP40010156-CH13-SW1
+//                        ctx.translateBy(x: 0, y: size.width)
+//                        ctx.scaleBy(x: 1, y: -1)
+//
+//                        // Draw
+//                        ctx.draw(masked, in: rect)
+//
+//                        // Restore context state
+//                        ctx.restoreGState()
+//                    }
+//                }
+//            }
+//        }
+//    }
+    
+    // We can do this as well
+    func drawMask() {
         
-        if let ctx: CGContext = UIGraphicsGetCurrentContext() {
+        if let cg: CGImage = self.image?.cgImage,
+           let size: CGSize = self.image?.size {
             
-            if let cg: CGImage = self.image?.cgImage,
-               let size: CGSize = self.image?.size {
-                
-                // If the mask is an image, then white areas are opaque, and black areas are transparent
-                // If the mas is a mask, white areas are transparent and black areas opaque.
-                // https://developer.apple.com/library/archive/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_images/dq_images.html#//apple_ref/doc/uid/TP30001066-CH212-TPXREF101
+            let renderer = UIGraphicsImageRenderer(size: size)
+            
+            let img = renderer.image { ctx in
                 
                 if let mask = UIImage(named: "tigermask_1_S")?.cgImage {
                     if let masked = cg.masking(mask) {
                         
                         let rect: CGRect = CGRect(origin: .zero, size: size)
-                        
+         
                         // Save context state
-                        ctx.saveGState()
+                        ctx.cgContext.saveGState()
                         
                         // Flip the context so that the coordinates match the default coordinate system of UIKit
                         // https://developer.apple.com/library/archive/documentation/2DDrawing/Conceptual/DrawingPrintingiOS/HandlingImages/Images.html#//apple_ref/doc/uid/TP40010156-CH13-SW1
-                        ctx.translateBy(x: 0, y: size.width)
-                        ctx.scaleBy(x: 1, y: -1)
+                        ctx.cgContext.translateBy(x: 0, y: size.width)
+                        ctx.cgContext.scaleBy(x: 1, y: -1)
                         
                         // Draw
-                        ctx.draw(masked, in: rect)
+                        ctx.cgContext.draw(masked, in: rect)
                         
                         // Restore context state
-                        ctx.restoreGState()
+                        ctx.cgContext.restoreGState()
                     }
                 }
             }
+            
+            self.image = img
+            
+            let view = UIImageView(image: self.image)
+            self.subviews.forEach { $0.removeFromSuperview() }
+            self.addSubview(view)
         }
     }
-    
 }
